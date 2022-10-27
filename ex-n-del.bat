cd507675be254d1c77c6dcb44670c80da2e834da
vignesh-seven <vignesh77024@gmail.com>
2022-10-27 16:32:34 +0530

    initial

---
+++ ex-n-del.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set filepath=%1
+
+@REM for /F "delims=" %%i in (%filepath%) do set dirname="%%~dpi" 
+@REM for /F "delims=" %%i in (%filepath%) do set filename="%%~nxi"
+for /F "delims=" %%i in (%filepath%) do set basename="%%~ni"
+
+@REM echo %filepath%
+@REM echo %dirname%
+@REM echo %filename%
+@REM echo %basename%
+
+mkdir %basename%
+
+echo File given: %filepath%
+
+"C:\Program Files\7-Zip\7z.exe" x %filepath% -o%basename%
+
+del %filepath%
+
